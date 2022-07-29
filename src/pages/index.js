c1c91b013f7ef207f4e9a529ec280d90374417d0
QuotumFED <99692563+QuotumFED@users.noreply.github.com>
2022-07-29 14:33:57 -0400

    Barreled Pages for navigation

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import About from './AboutPage'
+import Auth from './AuthWrapper'
+import Cart from './CartPage'
+import Checkout from './CheckoutPage'
+import Error from './ErrorPage'
+import Home from './HomePage'
+import PrivateRoute from './PrivateRoute'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+
+export { About, Auth, Cart, Checkout, Error, Home, PrivateRoute, Products, SingleProduct }
