72a30437a5edce94e4e8116b7be55c83012fc4f9
transcending92 <melaniekealacampbell@gmail.com>
2022-07-31 20:46:58 -0500

    Filter continue
    Continued working on the left section of the products filter page

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -58,6 +58,20 @@ if(action.type === UPDATE_FILTERS){
 if(action.type === FILTER_PRODUCTS){
   return {...state}
 }
+if(action.type === CLEAR_FILTERS){
+  return {...state,
+    filters: {
+      ...state.filters,
+      text:'',
+      company: 'all',
+      category: 'all',
+      color: 'all',
+      price: state.filters.max_price,
+      shipping: false,
+    },
+  }
+}
+
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
