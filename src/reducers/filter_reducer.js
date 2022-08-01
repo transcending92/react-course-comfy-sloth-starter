4607dbf6dacc6e41b4474e39e9838f24f4205d51
transcending92 <melaniekealacampbell@gmail.com>
2022-08-01 00:18:47 -0500

    Cart
    Cart component, about page, filter page

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -57,18 +57,46 @@ if(action.type === UPDATE_FILTERS){
   }
 if(action.type === FILTER_PRODUCTS){
   const {all_products} = state
-  const {text,category,company,color,price,shipping} = state.filters
-
-
+  const {text, category, company, color, price, shipping} = state.filters
 
   let tempProducts = [...all_products]
+  //filtering
+  //text
+
   if(text){
     tempProducts = tempProducts.filter((product) => {
-      return (
-        
-      )
+      return product.name.toLowerCase().startWith(text)
     })
   }
+
+  //category 
+  if(category !== 'all'){
+    tempProducts = tempProducts.filter(
+      (product) => product.category === category
+      )
+  }
+
+  //company
+  if(company !== 'all'){
+    tempProducts = tempProducts.filter(
+      (product) => product.company === company
+      )
+  }
+  //colors
+  if(color !== 'all'){
+    tempProducts = tempProducts.filter((product) => {
+        return product.colors.find((c) => c === color)
+      }
+    )
+  }
+//price
+  tempProducts = tempProducts.filter((product) => [product.price <= price])
+
+  //shipping
+  if(shipping){
+    tempProducts = tempProducts.filter((product) => product.shipping === true)
+  }
+
   return {...state, filtered_products: tempProducts }
 }
 if(action.type === CLEAR_FILTERS){
