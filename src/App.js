9baeece0d38314f5109ef3671cba132b75186cef
transcending92 <melaniekealacampbell@gmail.com>
2022-08-01 21:08:00 -0500

    Fixing bugs
    Fixing bugs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
-
+import AuthWrapper from './pages/AuthWrapper'
 import { About, 
         //  Auth, 
          Cart, 
          Checkout, 
          Error, 
          Home, 
-        //  PrivateRoute, 
+         PrivateRoute, 
          Products, 
          SingleProduct } from './pages'
 
@@ -16,21 +16,23 @@ import { About,
 
 function App() {
   return (
-  <Router>
-    <Navbar />
-    <Sidebar />
-    <Switch>
-      <Route exact path='/'><Home /></Route>
-      <Route exact path='/about'><About /></Route>
-      <Route exact path='/cart'><Cart /></Route>
-      <Route exact path='/products'><Products /></Route>
-      <Route exact path='/products/:id' children={<SingleProduct />} />
-      <Route exact path='/checkout'><Checkout /></Route> 
-      {/* Note: <Checkout /> will eventually be wrapped in the PrivateRoute */}
-      <Route path='*'><Error /></Route>
-    </Switch>
-    <Footer />
-  </Router>
+  <AuthWrapper>
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path='/'><Home /></Route>
+        <Route exact path='/about'><About /></Route>
+        <Route exact path='/cart'><Cart /></Route>
+        <Route exact path='/products'><Products /></Route>
+        <Route exact path='/products/:id' children={<SingleProduct />} />
+        <PrivateRoute exact path='/checkout'><Checkout /></PrivateRoute> 
+        {/* Note: <Checkout /> will eventually be wrapped in the PrivateRoute */}
+        <Route path='*'><Error /></Route>
+      </Switch>
+      <Footer />
+    </Router>
+  </AuthWrapper>
   )
 }
 
